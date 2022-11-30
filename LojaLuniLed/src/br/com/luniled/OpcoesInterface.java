ac75e2295ec199b5c52f1c53b9edb8758301ceb0
WilliRamon <acdc.ramon@hotmail.com>
2022-11-29 22:20:57 -0300

    Criando Interface

---
+++ LojaLuniLed/src/br/com/luniled/OpcoesInterface.java
@@ -0,0 +1,27 @@
+package br.com.luniled;
+
+public interface OpcoesInterface {
+	
+	public abstract void acesso();
+	
+	public abstract void consultarEstoque();
+	
+	public abstract void consultarClienteCadastrado();
+	
+	public abstract void realizarVenda();
+	
+	public abstract void consultarVendasRealizadas();
+	
+	public abstract void cadastrarProduto();
+	
+	public abstract void cadastrarCliente();
+	
+	public abstract void excluirProduto();
+	
+	public abstract void excluirCliente();
+	
+	public abstract void cancelarVenda();
+	
+	public abstract void totalVendido();
+
+}
